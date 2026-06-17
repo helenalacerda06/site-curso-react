81517c249e710636e684de789e61111e306009ec
helenalacerda06 <helenacardoso062009@gmail.com>
2026-06-17 17:32:56 -0300

    :)

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,9 +1,28 @@
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
 
 function App() {
   return (
-    <div className="App">
-     <p>Costs</p>
-    </div>
+    <>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contato">Contato</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/company" element={<Company />} />
+        <Route path="/contato" element={<Contact />} />
+        <Route path="/newproject" element={<NewProject />} />
+      </Routes>
+
+      <p>Footer</p>
+    </>
   );
 }
 
