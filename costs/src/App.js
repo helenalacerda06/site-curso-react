11d07e2df8f7a30a7942c5ed648477975c0a112f
helenalacerda06 <helenacardoso062009@gmail.com>
2026-06-19 16:01:47 -0300

    Estuturando o projeto. Obs: depois revisar o curso está desatualizado, por isso algumas coisas não estão como deveria

--- costs/src/App.js
+++ costs/src/App.js
@@ -3,23 +3,26 @@ import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
+import Container from './components/layout/Container';
 
 function App() {
   return (
     <>
-      <div>
+        <div>
         <Link to="/">Home</Link>
         <Link to="/contato">Contato</Link>
         <Link to="/company">Empresa</Link>
         <Link to="/newproject">Novo Projeto</Link>
       </div>
 
+ 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/company" element={<Company />} />
         <Route path="/contato" element={<Contact />} />
         <Route path="/newproject" element={<NewProject />} />
       </Routes>
+  
 
       <p>Footer</p>
     </>
